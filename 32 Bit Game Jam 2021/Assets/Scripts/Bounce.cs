fc11cd144f3952ebca6718579682e2a60aee56c3
ParanoidSyntaxError <71923508+ParanoidSyntaxError@users.noreply.github.com>
2021-09-17 23:33:58 +1000

    Removed broken slowing script

--- 32 Bit Game Jam 2021/Assets/Scripts/Bounce.cs
+++ 32 Bit Game Jam 2021/Assets/Scripts/Bounce.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
+	[SerializeField] private float bounceForce = 35;
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.GetComponent<PlayerMovement>() == true)
 		{
-			other.GetComponent<PlayerMovement>().AddForce(Vector3.up * 15);
+			other.GetComponent<PlayerMovement>().AddForce(Vector3.up * bounceForce);
 		}
 	}
 }
