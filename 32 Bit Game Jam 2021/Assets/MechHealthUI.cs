d05f20f6d45e046a2880c275c52b281777bd71cd
ParanoidSyntaxError <71923508+ParanoidSyntaxError@users.noreply.github.com>
2021-09-26 23:03:56 +1000

    Mech bear health bar

---
+++ 32 Bit Game Jam 2021/Assets/MechHealthUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MechHealthUI : MonoBehaviour
+{
+	public Health MechHealth;
+
+	public Image HPBar;
+	float startScale;
+
+	private void Awake()
+	{
+		startScale = HPBar.rectTransform.rect.width;
+	}
+
+	void Update()
+    {
+		if(Vector3.Distance(Camera.main.transform.position, MechHealth.transform.position) <= 80)
+		{
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				transform.GetChild(i).gameObject.SetActive(true);
+			}
+
+			Debug.Log((float)(MechHealth.HP / MechHealth.MaxHP));
+
+			HPBar.transform.localScale = new Vector3((float)MechHealth.HP / (float)MechHealth.MaxHP, 1, 1);		
+		}
+		else
+		{
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				transform.GetChild(i).gameObject.SetActive(false);
+			}
+		}
+    }
+}
