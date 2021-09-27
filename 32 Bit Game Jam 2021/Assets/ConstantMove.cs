04006ca2143c55da40b4d365885362dc329fc7dc
Gridbear7 <58487913+Gridbear7@users.noreply.github.com>
2021-09-26 21:38:13 -0400

    recommiting stashed changes?

--- 32 Bit Game Jam 2021/Assets/ConstantMove.cs
+++ 32 Bit Game Jam 2021/Assets/ConstantMove.cs
@@ -6,12 +6,10 @@ public class ConstantMove : MonoBehaviour
 {
 	public Vector3 PositionAmount;
 	public Vector3 RotationAmount;
-	public Vector3 ScaleAmount;
 
     void FixedUpdate()
     {
-		transform.Rotate(RotationAmount * Time.deltaTime);
-		transform.position = transform.position + PositionAmount * Time.deltaTime;
-		transform.localScale = transform.localScale + ScaleAmount * Time.deltaTime;
+		transform.Rotate(RotationAmount);
+		transform.position = transform.position + PositionAmount;
     }
 }
